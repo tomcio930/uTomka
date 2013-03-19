1019d732307293cd695566b87bdecb5eaabe3b84
tgorny <tomasz.gorny@comarch.pl>
2013-03-19 17:18:15 +0100

    add bootstrap

--- media/models.py
+++ media/models.py
@@ -7,12 +7,4 @@ def get_image_path(instance, filename):
 class Image(models.Model):
     image = models.ImageField(upload_to=get_image_path)
     
-    def delete(self, *args, **kwargs):
-        # You have to prepare what you need before delete the model
-        storage, path = self.image.storage, self.image.path
-        # Delete the model before the file
-        super(Image, self).delete(*args, **kwargs)
-        # Delete the file after the model
-        storage.delete(path)
-    
     
